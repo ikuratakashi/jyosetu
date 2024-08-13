4250e2bed4acf2688de5819d7ce1f357356c9fec
Takashi Ikura <ikura1108@icloud.com>
2024-08-13 11:50:59 +0900

    名もなき修正

--- momo_ed.sh
+++ momo_ed.sh
@@ -3,17 +3,26 @@ if [ -f /tmp/momo.pid ]; then
     kill -9 $(cat /tmp/momo.pid)
     rm /tmp/momo.pid
 
+    echo ""
     echo "※プロセス終了できなかった場合"
-    ps -ef | grep momo
-    echo "最初の方の番号がプロセスIDです。"
-    echo "sudo kill プロセスID のコマンドで手動削除してください。"
-
-else
-    echo "PID file not found. Is momo running?"
+    echo ""
 
     ps -ef | grep momo
     
+    echo ""
     echo "最初の方の番号がプロセスIDです。"
-    echo "sudo kill プロセスID のコマンドで手動削除してください。"
+    echo "sudo kill -9 <プロセスID> のコマンドで手動削除してください。"
+    echo "例）sudo kill -9 1234"
+
+else
+    echo "PID file not found. Is momo running?"
+    echo ""
+
+    ps -ef | grep momo
+    
+    echo ""
+    echo "最初の方の番号がプロセスIDです。"
+    echo "sudo kill -9 <プロセスID> のコマンドで手動削除してください。"
+    echo "例）sudo kill -9 1234"
 
 fi
