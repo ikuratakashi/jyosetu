ba673154950f67c19fc4d535e7992bc49b8da1f0
Takashi Ikura <ikura1108@icloud.com>
2024-08-13 10:11:17 +0900

    名もなき修正

--- sv.py
+++ sv.py
@@ -3,6 +3,7 @@ sys.path.append('lib')
 import asyncio
 import websockets # type: ignore
 
+'''
 async def handler(websocket, path):
     async for message in websocket:
         print(f"Received: {message}")
@@ -12,3 +13,20 @@ start_server = websockets.serve(handler, "localhost", 8080)
 asyncio.get_event_loop().run_until_complete(start_server)
 print("WebSocket server is running on ws://localhost:8080")
 asyncio.get_event_loop().run_forever()
+
+'''
+
+import subprocess
+
+# momoの実行コマンドとオプション
+command = ['./momo', '--no-audio-device', 'test']
+
+# subprocessを使ってコマンドを実行
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# 標準出力と標準エラーを取得
+stdout, stderr = process.communicate()
+
+# 出力を表示
+print(stdout.decode())
+print(stderr.decode())
