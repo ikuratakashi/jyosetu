3b143f0e376b25ce18c3e59069cbace0a27a2009
Takashi Ikura <ikura1108@icloud.com>
2024-08-20 22:19:50 +0900

    gitのプルを強制的に実行するシェルを追加

--- gitpull.sh
+++ gitpull.sh
@@ -1,4 +1,26 @@
-git fetch origin
-git reset --hard origin/develop-ikura
-git reset --hard origin/main
-chmod 777 init.sh
+#!/bin/bash
+git branch
+echo "現在のブランチにdevelop-ikuraを強制的に上書きします。"
+echo "よろしいですか？ (y/n)"
+read answer
+
+if [ "$answer" = "y" ]; then
+    git fetch origin
+    git reset --hard origin/develop-ikura
+    chmod 777 init.sh
+else
+    echo "処理を中止しました。"
+    exit
+fi
+
+echo "続けてmomoなどを実行できるように設定します。"
+echo "よろしいですか？ (y/n)"
+read answer
+
+if [ "$answer" = "y" ]; then
+    ./init.sh
+else
+    echo "処理を中止しました。"
+    exit
+fi
+
