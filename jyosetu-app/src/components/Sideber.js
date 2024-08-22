caeb9d450d11c88466d3bbc68d66e7016870c89d
Takashi Ikura <ikura1108@icloud.com>
2024-08-22 21:32:22 +0900

    サイドバー追加開発開始

---
+++ jyosetu-app/src/components/Sideber.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Sideber() {
+  return (
+    <div className='Sidebar'>
+        <ul className='SidebarList'>
+        </ul>
+    </div>
+  )
+}
+
+export default Sideber
