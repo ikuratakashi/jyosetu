7b113f0359e411c40bdfb1bf11ab0ae42bdef64a
Takashi Ikura <ikura1108@icloud.com>
2024-09-09 00:03:02 +0900

    名もなき修正

--- sv.py
+++ sv.py
@@ -1,15 +1,40 @@
+version = "10.2.101"
+PgName = "Jyosetu Message Server"
+
 import sys
 sys.path.append('lib')
 import asyncio
 import websockets  # type: ignore
 import socket
+import os
+import platform
 
 port = 50001
 host = "0.0.0.0"  # すべてのインターフェースから接続を受け入れる
 
+def Openning():
+    print('/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/')
+    print('')
+    print(f'{PgName}')
+    print('')
+    print(f'var:{version}')
+    print('')
+    print('/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/')
+    print('')
+    print('')
+    print(f'Wellcome to {PgName}.')
+    print('')
+    print('')
+
 async def handler(websocket, path):
     try:
         async for message in websocket:
+
+            if platform.system() == "Windows":
+                os.system("cls")
+            else:
+                os.system("clear")
+
             print(f"Received message: {message}")
             #await websocket.send(f"Echo: {message}")
     except websockets.exceptions.ConnectionClosedError as e:
@@ -28,4 +53,5 @@ async def main():
     await start_server.wait_closed()
 
 if __name__ == "__main__":
+    Openning()
     asyncio.run(main())
