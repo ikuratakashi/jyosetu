37b4faaa0e4f1e8b9dfa0e4b0262e621ef5ad892
Takashi Ikura <ikura1108@icloud.com>
2024-10-02 07:33:37 +0900

    センサークラス追加

--- Censor.py
+++ Censor.py
@@ -1,3 +1,4 @@
+import threading
 from env import clsEnvData
 from Error import clsError
 from Log import clsLog
@@ -18,9 +19,9 @@ class clsCensor_Micro(clsLog,clsError):
     スレッドの終了フラグ
     '''
 
-    Log : clsLog = clsLog()
+    Thred : threading.Thread = None
     '''
-    ログ
+    処理のスレッド
     '''
 
     def __init__(self):
@@ -38,8 +39,19 @@ class clsCensor_Micro(clsLog,clsError):
         '''
         クローズ
         '''
+        self.IsThredStop = True
+        if self.Thred != None:
+            self.Thred.join()
+            self.Thred = None
 
-    def start(self):
+    def Start(self):
+        '''
+        クラッチコマンドを送信するスレッド
+        '''
+        self.Thred = threading.Thread(target=self.MicroThred)
+        self.Thred.start()
+
+    def MicroThred(self):
         '''
         センサー受信の開始
         '''
@@ -52,9 +64,45 @@ class clsCensor_Micro(clsLog,clsError):
                 self.onReceive(data)
             except Exception as e:
                 self.HandleError(cur,f"Unexpected error:{e}")
+
     
     def onReceive(self,pData):
         '''
         受信のイベント
         '''
         pass
+
+class clsCensorManager(clsLog,clsError):
+    '''
+    センサー マネージャー
+    '''
+
+    EnvData : clsEnvData = None
+    '''
+    設定
+    '''
+
+    IsThredStop : bool = False
+    '''
+    スレッドの終了フラグ
+    '''
+
+    Censor_Micro : clsCensor_Micro = None
+    '''
+    センサー マイクロ波
+    '''
+
+    def __init__(self):
+        '''
+        コンストラクタ
+        '''
+        self.EnvData = clsEnvData()
+    
+    def Start(self):
+        '''
+        各種センサーのスタート
+        '''
+
+        # マイクロ波センサー
+        self.Censor_Micro = clsCensor_Micro()
+        self.Censor_Micro.start()
